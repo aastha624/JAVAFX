d51163da9ebb5f390dacc6a7967ce9fce9507309
aastha624 <patelaastha2910@gmail.com>
2024-06-14 23:28:26 +0530

    learn choicebox create concept

---
+++ choiceboxconcept/src/controller.java
@@ -0,0 +1,34 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+
+public class controller implements Initializable{
+
+	@FXML
+	private Label myLabel;
+	
+	@FXML
+	private ChoiceBox<String> myChoiceBox;
+	
+	private String[] food = {"pizza","Vadapau","Burger"};
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		myChoiceBox.getItems().addAll(food);
+		myChoiceBox.setOnAction(this::getFood);
+		
+	}
+	
+	public void getFood(ActionEvent event) {
+		
+		String myFood = myChoiceBox.getValue();
+		myLabel.setText(myFood);
+	}
+
+}
